99dcf7771bfb330b6adc65ae0c3ea58c2b62d94c
scrampin <steph.crampin@gmail.com>
2017-02-28 07:54:02 +0000

    refactored directory with array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,28 @@
 student_count = 11
 
-puts "The students of Villains Academy"
-puts "----------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+#let's put all the students into an array
 
-puts "Overall, we have #{student_count} great students"
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates",
+]
+
+#and them print them
+puts "The students of Villains Academy"
+puts "-----------"
+students.each do |student|
+  puts student
+end
+
+#finally, we print the total
+
+puts "Overall, we have #{students.count} great students"
