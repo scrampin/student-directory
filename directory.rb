4b6c6d3813c091850c60b2339c95bc014f5ca916
scrampin <steph.crampin@gmail.com>
2017-02-28 08:29:30 +0000

    added user input

--- directory.rb
+++ directory.rb
@@ -1,20 +1,38 @@
 student_count = 11
 
-#let's put all the students into an array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {:name => name, :cohort => :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
 
-students = [
-            {:name => "Dr. Hannibal Lecter", :cohort => :november},
-            {:name => "Darth Vader", :cohort => :november},
-            {:name => "Nurse Ratched", :cohort => :november},
-            {:name => "Michael Corleone", :cohort => :november},
-            {:name => "Alex DeLarge", :cohort => :november},
-            {:name => "The Wicked Witch of the West", :cohort => :november},
-            {:name => "Terminator", :cohort => :november},
-            {:name => "Freddy Krueger", :cohort => :november},
-            {:name => "The Joker", :cohort => :november},
-            {:name => "Joffrey Baratheon", :cohort => :november},
-            {:name => "Norman Bates", :cohort => :november},
-]
+
+#students = [
+#            {:name => "Dr. Hannibal Lecter", :cohort => :november},
+#            {:name => "Darth Vader", :cohort => :november},
+#            {:name => "Nurse Ratched", :cohort => :november},
+#            {:name => "Michael Corleone", :cohort => :november},
+#            {:name => "Alex DeLarge", :cohort => :november},
+#            {:name => "The Wicked Witch of the West", :cohort => :november},
+#            {:name => "Terminator", :cohort => :november},
+#            {:name => "Freddy Krueger", :cohort => :november},
+#            {:name => "The Joker", :cohort => :november},
+#            {:name => "Joffrey Baratheon", :cohort => :november},
+#            {:name => "Norman Bates", :cohort => :november},
+#]
 
 def print_header
   puts "The students of Villains Academy"
@@ -33,6 +51,7 @@ end
 
 #nothing happens until we call the methods
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
